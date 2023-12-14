8253c503aebac4699530619d560ef3a04714f9d7
DaviALacerda <daviallac@gmail.com>
2023-12-14 17:07:19 -0300

    Beta Commit 2 - adding googles

---
+++ src/main/java/net/jorgin/bellanmod/item/BellAnItems.java
@@ -0,0 +1,20 @@
+package net.jorgin.bellanmod.items;
+
+import net.jorgin.bellanmod.BellAnMod;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class BellAnItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, BellAnMod.MOD_ID);
+
+    public static final RegistryObject<Item> Googles = ITEMS.register("googles",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
